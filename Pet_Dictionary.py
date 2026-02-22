273544b8ed8fc9e7f980d64efdfa68a5aecf098d
Gaven <ghuynh@student.cscc.edu>
2026-02-22 15:52:00 -0500

    Pet dictionaries

---
+++ Pet_Dictionary.py
@@ -0,0 +1,20 @@
+pet1 = {
+    "animal": "Lizard",
+    "owner": "Lizzy"
+}
+
+pet2 = {
+    "animal": "Turtle",
+    "owner": "Turner"
+}
+
+pet3 = {
+    "animal": "Fish",
+    "owner": "Finnly"
+}
+
+pet4 = {
+    "animal": "Salamander",
+    "owner": "Sally"
+}
+
